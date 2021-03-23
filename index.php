1c98c481db2672f99e9990fb85a040df044fe5b1
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-03-23 14:32:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Lista de escolhas </title>
+</head>
+<body>
+	<nav>
+		<li> <a href="/crud/formulario/form_inserir.php"> Inserir </a> </li>
+		<li> <a href="/crud/formulario/form_listar.php"> Listar </a> </li>
+		<li> <a href="/crud/formulario/form_atualizar.php"> Atualizar </a> </li>
+		<li> <a href="/crud/formulario/form_excluir.php"> Excluir </a> </li>
+	</nav>
+</body>
+</html>
