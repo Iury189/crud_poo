561b46c871ecd01125adbe00cf3ba3be4c41d05e
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-03-25 20:44:01 -0300

    Add files via upload

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -12,7 +12,7 @@
 
 				try {
 
-					self::$instancia = new PDO('mysql:host=' . HOST . '; dbname=' . BASE, USER, PASS);
+					self::$instancia = new PDO('mysql:host=' . HOST . ';dbname=' . BASE . ';charset=utf8', USER, PASS);
 					self::$instancia->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 					self::$instancia->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
